9b74e03ae7d08c7e819a8a4f353fe1bd9acf2a3b
Sergey Kolesnik <dword@yandex-team.ru>
2012-10-26 19:57:25 +0400

    NEW: first version

---
+++ switch_case.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+import sublime
+import sublime_plugin
+
+
+def detect_case(text):
+    '''
+        Если текст состоит из букв, цифр и _, то это underscore.
+        Если текст состоит из букв, цифр, то это camel.
+    '''
+    replaced = text.replace('_', '')
+    if not replaced.isalnum():
+        return 'other'
+
+    if text.find('_') != -1:
+            return 'underscore'
+    return 'camel'
+
+
+def translate_to_underscore_case(text):
+    underscored = ''
+    for ch in text:
+        if ch.isupper():
+            underscored += '_'
+        underscored += ch
+    return underscored.lower().strip('_')
+
+
+def translate_to_camel_case(text):
+    words = map(lambda x: x.capitalize(), text.split('_'))
+    words[0] = words[0].lower()
+    return ''.join(words)
+
+
+class SwitchCaseCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for region in self.view.sel():
+            text = self.view.substr(region)
+            if not text:
+                return
+
+            case = detect_case(text)
+            if case == 'camel':
+                text = translate_to_underscore_case(text)
+            elif case == 'underscore':
+                text = translate_to_camel_case(text)
+            else:
+                sublime.status_message('Unknown case type')
+                return
+
+            self.view.replace(edit, region, text)
